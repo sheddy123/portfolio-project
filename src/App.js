26bfde7fd3eb3a3977770b0d119666126d2750a9
Ifeanyi Odom <iodom@appzonegroup.com>
2021-08-14 09:31:58 +0100

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar/Navbar';
+import Header from './components/Header/Header';
+import Particles from 'react-particles-js';
 function App() {
   return (
     <>
+      <Particles
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900,
+              },
+            },
+            shape: {
+              type: 'circle',
+              stroke: {
+                width: 6,
+                color: '#f9ab00',
+              },
+            },
+          },
+        }}
+      />
       <Navbar />
+      <Header />
     </>
   );
 }
